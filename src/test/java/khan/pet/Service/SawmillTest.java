698787ea5e3700a1eba025ba4aaa9de4e60b4d3b
PepperWorm <168024258+PepperWorm@users.noreply.github.com>
2024-11-23 00:39:24 +0300

    dell unused imports

--- src/test/java/khan/pet/Service/SawmillTest.java
+++ src/test/java/khan/pet/Service/SawmillTest.java
@@ -4,8 +4,6 @@ import khan.pet.entity.Blank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
